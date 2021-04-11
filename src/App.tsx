cb28752189809ae8067b64d3dfac3367a2ac980d
zinirun <heojj97@naver.com>
2021-04-11 17:46:55 +0900

    modify: add router and switch to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Rooms from "./Rooms";
+import Chat from "./Chat";
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/room/:roomId" component={Chat} />
+        <Route path="/" component={Rooms} />
+      </Switch>
+    </Router>
+  );
 }
