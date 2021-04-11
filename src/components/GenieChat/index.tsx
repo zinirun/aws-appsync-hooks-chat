78b5d7bcd8863db7588d8cbdfb11ab7743178f29
zinirun <heojj97@naver.com>
2021-04-11 23:51:07 +0900

    modify: chat component

--- src/components/GenieChat/index.tsx
+++ src/components/GenieChat/index.tsx
@@ -1,24 +1,25 @@
 import { Button, TextField } from "@material-ui/core";
 import { useCallback, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 
 export default function GenieChat({ messages, onSend, user }: any) {
-  const [message, setMessage] = useState("");
-  const handleMessageChange = useCallback((e) => {
-    setMessage(e.target.value);
+  const [text, setText] = useState("");
+  const handleTextChange = useCallback((e) => {
+    setText(e.target.value);
   }, []);
   const handleSubmit = useCallback(
     (e: any) => {
       e.preventDefault();
       onSend({
-        text: message,
+        text,
         user,
         createdAt: new Date(),
         id: uuidv4(),
       });
-      setMessage("");
+      setText("");
     },
-    [message, onSend, user]
+    [text, onSend, user]
   );
   return (
     <div>
@@ -26,14 +27,16 @@ export default function GenieChat({ messages, onSend, user }: any) {
         .sort((message: any) => message.id)
         .map((message: any) => (
           <p key={`genie-chat-message-${message.id}`}>
-            {message.owner}: {message.content} ({message.createdAt})
+            {message.user.name}: {message.text} (
+            {moment(message.createdAt).format("YYYY-MM-DD HH:mm:ss")})
           </p>
         ))}
       <form noValidate onSubmit={handleSubmit}>
         <TextField
-          name="message"
-          value={message}
-          onChange={handleMessageChange}
+          name="text"
+          value={text}
+          onChange={handleTextChange}
+          autoComplete="off"
           autoFocus
         />
         <Button type="submit" color="primary" variant="contained">
